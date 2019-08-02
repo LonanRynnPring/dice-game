82950df4c7afff83ef5d9df4419d6f2399baa0a4
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-02 10:50:16 +0100

    added a current score and a marker to show who is the current player. Updated rules

--- script2.js
+++ script2.js
@@ -3,8 +3,14 @@ const image = document.querySelector("#image");
 const button = document.querySelector("#button");
 const score2 = document.querySelector("#score2");
 const hold = document.querySelector("#hold");
+const currentScore = document.querySelector("#currentScore");
+const play1 = document.querySelector("#play1");
+const play2 = document.querySelector("#play2");
+
 let player = score;
 
+play1.style.color = "white";
+play1.style.textDecoration = "underline";
 
 class Dice{
     constructor(value, link){
@@ -28,24 +34,52 @@ const six = new Dice(6, "img/dice6.png")
 
 diceArray = [one, two, three, four, five, six];
 
-
-
-button.addEventListener("click", ()=>{
-    image.style.display = "inline-block";
-    let num = Math.floor(Math.random() * 6); 
-    image.src = diceArray[num].link;
-    player.textContent = parseInt(player.textContent)+parseInt(diceArray[num].value);
+function check () {
     if (parseInt(player.textContent) >= 20){
         alert(`You win, your score was ${player.textContent}`);
         score.textContent = 0;
         score2.textContent = 0;
         image.style.display = "none";
     }
-    else if (diceArray[num].value == 1){
-        alert("You lose");
-        score.textContent = 0;
-        score2.textContent = 0;
-        image.style.display = "none";
+}
+
+function active () {
+    if (player == score){
+        play1.style.color = "white";
+        play1.style.textDecoration = "underline";
+        play2.style.color = "black";
+        play2.style.textDecoration = "none";
+    }
+    else {
+        play2.style.color = "white";
+        play2.style.textDecoration = "underline";
+        play1.style.color = "black";
+        play1.style.textDecoration = "none";
+    }
+}
+
+
+button.addEventListener("click", ()=>{
+    image.style.display = "inline-block";
+    let num = Math.floor(Math.random() * 6); 
+    image.src = diceArray[num].link;
+    // player.textContent = parseInt(player.textContent)+parseInt(diceArray[num].value);
+    currentScore.textContent = parseInt(currentScore.textContent)+parseInt(diceArray[num].value);
+    
+    if (diceArray[num].value == 1){
+        // alert("You lose");
+        // score.textContent = 0;
+        // score2.textContent = 0;
+        // image.style.display = "none";
+        currentScore.textContent = 0;
+        if (player == score){
+            player = score2;
+            active()
+        }
+        else {
+            player = score;
+            active()
+        }
     }
     
 })
@@ -54,9 +88,18 @@ button.addEventListener("click", ()=>{
 
 hold.addEventListener("click",()=>{
     if (player == score){
+        score.textContent = parseInt(score.textContent)+parseInt(currentScore.textContent)
+        check()
         player = score2
+        active()
+        currentScore.textContent = 0
     }
     else {
+        score2.textContent = parseInt(score2.textContent)+parseInt(currentScore.textContent)
+        check()
         player = score
+        active()
+        currentScore.textContent = 0
     }
 })
+
